be701531de756288df06195a054c0ae62b3436ad
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2023-03-11 23:48:38 +0330

    add new useState class with constructor

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 console.log("arman");
 
 type setter = (callFunc: any) => any;
+
+class useState {
+  private initialValue: (string | number)[];
+  constructor() {
+    this.initialValue = [];
+  }
+}
