83a3095236303baebb7e44c2e6963875148e8ab4
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2023-03-15 02:51:12 +0330

    comments changed

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 function useState(stateVal: any) {
   /// warning state must be const casue it's value cannot change manually
   class useStateClass {
     #privateState: any = stateVal;
     constructor() {}
-=======
-///arman code 
-console.log("arman");
->>>>>>> fc8bfb074f1d9ee24cf0a389f15d2246ea89f29b
 
     get state(): any {
       return this.#privateState;
@@ -27,14 +22,7 @@ console.log("arman");
   return [getState, setState];
 }
 
-<<<<<<< HEAD
 const [count, setCount]: any = useState([12, 13]);
 console.log(count());
 setCount({ color: "#DEFFEE" });
 console.log(count());
-=======
-//let [static,setStatic] = new useState2();
-
-// console.log(a);
-
->>>>>>> fc8bfb074f1d9ee24cf0a389f15d2246ea89f29b
