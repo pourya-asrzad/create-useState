029e9a146cbaea94775a5210e18d4f48d595f2d7
puria-asr <poriaasrzad@gmail.com>
2023-03-15 12:14:37 +0330

    useState base created

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("object");
+function useState(initalValue) {
+    let state = initalValue;
+    function setState(inputValue = state) {
+        return state = inputValue;
+    }
+    return [state, setState];
+}
+const [state, setState] = useState(1);
+console.log();
+setState(2);
+console.log(state);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log("object");
+
+
+function useState(initalValue:any):[any,Function]{
+
+  let state = initalValue;
+
+function setState(inputValue:any=state){
+   return state = inputValue;
+  }
+
+  return [state,setState];
+}
+
+
+const [state,setState]= useState(1);
+console.log();
+setState(2)
+
+console.log(state);
